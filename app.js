c6f3dc216bec0cacd6b0e517af7c1bb1e5ad7bd6
Prada Tech <pradatech@outlook.com>
2021-01-05 11:28:49 -0500

    started project added JS object

---
+++ app.js
@@ -0,0 +1,10 @@
+var airplane = {
+    Name: 787,
+    Manufacture: 'Boeing',
+    range: 7500,
+    seatCapacity: 350,
+    price: 167000000,
+    fuselage: 'widebody',
+    LD3_Cargo: 36,
+    costPerMile: 3.33,
+}
